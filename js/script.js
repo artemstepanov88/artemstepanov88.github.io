14e7deaea7b8b746f8681edb5c13bfc4e1f9cffd
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-04-11 11:36:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ let logo = document.querySelector('.logo');
 
 logo.addEventListener('mouseover', function(){
     document.querySelector('.logo').style.opacity = 0.2; 
+         document.querySelector('.logo480px').style.width = 30%; 
+                document.querySelector('.logo480px').style.height = 30%; 
+        
    
 })
     logo.addEventListener('mouseout', function(){
         document.querySelector('.logo').style.opacity = 0.3; 
-                document.querySelector('.logo480px').style.width = 30%; 
-                document.querySelector('.logo480px').style.height = 30%; 
-        
+           
 })
 logo.addEventListener('mouseover', function(){
 
