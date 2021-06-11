ec44c19d0ae6af6a66c98888966c1ac7d59d6d02
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-06-11 20:15:57 +0300

    Add files via upload

---
+++ js/script-skills.js
@@ -0,0 +1,6 @@
+let skills = document.querySelector('.skills-title');
+skills.addEventListener('mouseover', function(){
+    document.querySelector('.skills-title').textContent = "Junior Front-End developer";
+     
+ console.log("Работатет 11.06.21")
+} )
