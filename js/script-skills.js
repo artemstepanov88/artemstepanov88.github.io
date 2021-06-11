672245e76ca7464b8ece4f5b8e45fae5dca23bf7
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-06-11 20:37:37 +0300

    Update script-skills.js

--- js/script-skills.js
+++ js/script-skills.js
@@ -1,6 +1,16 @@
 let skills = document.querySelector('.skills-title');
 skills.addEventListener('mouseover', function(){
-    document.querySelector('.skills-title').textContent = "Junior Front-End developer";
-     
- console.log("Работатет 11.06.21")
+    document.querySelector('.skills-title').textContent = "НАВЫКИ";
+    
 } )
+    skills.addEventListener('mouseout', function(){
+            document.querySelector('.skills-title').textContent = "Навыки";
+        } )
+let jquery = document.querySelector('.jquery_skills');
+
+   jquery.querySelector('.jquery_skills').style.height = 10%;
+    
+} )
+    jquery.addEventListener('mouseout', function(){
+            document.querySelector('.jquery_skills').style.height = 20%;
+        } )
