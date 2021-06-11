f9b85340b9b52a486565fb21a6c62d31e4c03725
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-06-11 07:58:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,10 +36,10 @@ let j = document.querySelector('.logo_jquery');
 j.addEventListener('mouseover', function(){
     document.querySelector('.logo_jquery').style.width = '10%';
       document.querySelector('.logo_jquery').style.height = '10%';
- 
+ console.log("Работатет 11.06.21")
 } )
 
 j.addEventListener('mouseout', function(){
     document.querySelector('.logo_jquery').style = 'width: 20%; height: 20%;';
- 
+  console.log("Работатет 11.06.21 ++")
 } )
