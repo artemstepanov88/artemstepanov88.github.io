01e5c8c612f36b43e9b7a5199dfcf438826c06b8
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-07-17 19:04:09 +0300

    Update script.mjs

--- js/script.mjs
+++ js/script.mjs
@@ -9,14 +9,14 @@ import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/thre
       canvas,
       alpha: true,
     });
-  const fov = 20;
+  const fov = 10;
   const aspect = 2;  // the canvas default
   const near = 0.1;
   const far = 100;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.x = 0;
-    camera.position.y = 80;
-    camera.position.z = 80;
+    camera.position.y = 60;
+    camera.position.z = 60;
 
     // Controls
   const controls = new OrbitControls(camera, canvas);
