7cbdf3bb9d2dc248689680b9face25c58381ac7f
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-07-17 21:27:54 +0300

    Update script.mjs

--- js/script.mjs
+++ js/script.mjs
@@ -9,7 +9,7 @@ import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/thre
       canvas,
       alpha: true,
     });
-  const fov = 10;
+  const fov = 45;
   const aspect = 2;  // the canvas default
   const near = 0.1;
   const far = 100;
