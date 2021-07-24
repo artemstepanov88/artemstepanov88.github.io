f75e28e732fa44b2c61c307c5b3bb8061bfb9195
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-07-24 18:43:54 +0300

    Update script.mjs

--- js/script.mjs
+++ js/script.mjs
@@ -14,7 +14,7 @@ import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/thre
   const near = 0.1;
   const far = 100;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.x = 90;
+  camera.position.x = 10;
     camera.position.y = 50;
     camera.position.z = 50;
 
