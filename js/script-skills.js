760a47ce8808b26a2a8963f00526e332cf051dac
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-08-23 19:47:22 +0300

    Update script-skills.js

--- js/script-skills.js
+++ js/script-skills.js
@@ -21,8 +21,8 @@ let jquery = document.querySelector('img.logo_jquery');
         } )
 let skills_logo = document.querySelector('img.logo_skills');
     skills_logo.addEventListener('mouseover', function(){
-         
-            document.querySelector('img.logo_skills').style.transition = "all 2s";
+         document.querySelector('img.logo_jquery').style.transform = "rotate:(50deg)";
+         document.querySelector('img.logo_skills').style.transition = "all 2s";
        
 } )
     skills_logo.addEventListener('mouseout', function(){
