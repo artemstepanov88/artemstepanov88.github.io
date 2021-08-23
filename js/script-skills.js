20a00e6d079785dc5042e2ebc926e8ea64b8fa28
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-08-23 18:52:44 +0300

    Update script-skills.js

--- js/script-skills.js
+++ js/script-skills.js
@@ -19,3 +19,13 @@ let jquery = document.querySelector('img.logo_jquery');
     
         document.querySelector('img.logo_jquery').style.transform = "rotate(20deg)";
         } )
+let jquery = document.querySelector('img.logo_skills');
+    jquery.addEventListener('mouseover', function(){
+         
+            document.querySelector('img.logo_skills').style.transition = "all 2s";
+       
+} )
+    jquery.addEventListener('mouseout', function(){
+    
+        document.querySelector('img.logo_skills').style.transform = "rotate(-20deg)";
+        } )
