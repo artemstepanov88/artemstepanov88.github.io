75ffc0a71c347969c576d0e6ddbcba838179daeb
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-08-24 12:45:17 +0300

    Update script-skills.js

--- js/script-skills.js
+++ js/script-skills.js
@@ -44,11 +44,11 @@ let skills_logo = document.querySelector('img.logo_skills');
 
 let my_gif = document.querySelector('img.my-logo');
     my_gif.addEventListener('mouseover', function(){
-      document.querySelector('img..my-logo').style.transform = "rotate(360deg)";
-         document.querySelector('img..my-logo').style.transition = "all 1s";
+      document.querySelector('img.my-logo').style.transform = "rotate(360deg)";
+         document.querySelector('img.my-logo').style.transition = "all 1s";
          
 } )
    my_gif.addEventListener('mouseout', function(){
    
-       document.querySelector('img..my-logo').style.transform = "rotate(360deg)";
+       document.querySelector('img.my-logo').style.transform = "rotate(360deg)";
         } )
