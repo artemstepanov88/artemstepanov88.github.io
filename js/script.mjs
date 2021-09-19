195ffe8062bf9dcdee1704538c0fbce9a3537962
artemstepanov88 <66906600+artemstepanov88@users.noreply.github.com>
2021-09-19 11:08:11 +0300

    Update script.mjs

--- js/script.mjs
+++ js/script.mjs
@@ -31,7 +31,57 @@ import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/thre
 
   const cubes = [];  // just an array we can use to rotate the cubes
   const loader = new THREE.TextureLoader();
+  
+  
+  
+  
+// 3d model test !!!!!!!!!!!!!!
+  
+  const loader = new GLTFLoader();
 
+
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'img/yourMesh.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+  
+  
+  
+  
+
+  
+  
+  //  3d model test !!!!!!!!!!!!!
+  
+  
+  
+  
   const materials = [
     new THREE.MeshBasicMaterial({map: loader.load('img/1.jpg')}),
     new THREE.MeshBasicMaterial({map: loader.load('img/2.jpg')}),
